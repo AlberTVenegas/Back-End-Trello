e9e737427a416d86dedecef6c30ebf68e603f6f5
Albert Venegas <albertvenegas24@gmail.com>
2025-01-14 15:19:09 -0300

    fix home page

--- Trello_Back_End/urls.py
+++ Trello_Back_End/urls.py
@@ -20,6 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('trello', include('Trello.urls')),
         # YOUR PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
